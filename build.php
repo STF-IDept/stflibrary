3518892c963c3808785c345a22f9dec8c63967c4
Larry Garfield <larry@garfieldtech.com>
2008-11-08 08:57:31 +0000

    Fixed conflict.

--- build.php
+++ build.php
@@ -1,42 +1,51 @@
 <?php
 
-set_include_path(get_include_path() . PATH_SEPARATOR . '/usr/share/xml/docbook/stylesheet/nwalsh');
+//set_include_path(get_include_path() . PATH_SEPARATOR . '/usr/share/xml/docbook/stylesheet/nwalsh');
 
-// Load the XML source
-$xml = new DOMDocument;
-$xml->load('src/set.xml');
+define('SCRIPT_DIR', dirname(__FILE__));
+define('SRC_DIR', SCRIPT_DIR . '/src');
+define('STYLESHEET_DIR', SCRIPT_DIR . '/xsl');
+define('DOCBOOK_STYLESHEET_DIR', STYLESHEET_DIR . '/docbook');
 
-//print_r($xml->saveXML());
-$xml->xinclude();
+function process() {
 
-//print_r($xml->saveXML());
+  // Load the XML source
+  $xml = new DOMDocument;
+  $xml->load(SRC_DIR . '/set.xml');
+  
+  // Interpolate all XInclude directives.
+  $xml->xinclude();
+  
+  // Load the XSLT stylesheet.
+  $xsl = new DOMDocument;
+  $xsl->load(DOCBOOK_STYLESHEET_DIR . '/html/docbook.xsl');
+  
+  // Configure the transformer
+  $proc = new XSLTProcessor;
+  if (!$proc->hasExsltSupport()) {
+  	throw new Exception('EXSLT Support not available.');
+  }
+  $proc->importStyleSheet($xsl); // attach the xsl rules
+  
+  
+  //file_put_contents('test.html', $proc->transformToXml($xml));
+  
+  
+  
+  $book = $xml->getElementById('tm');
+  
+  var_dump($book);
+  
+  var_dump(gettype($book));
+  
+  file_put_contents('test-book.html', $proc->transformToXml($book));
+  
+
+}
 
 
-$xsl = new DOMDocument;
-//$xsl->load('xsl/html-set.xsl');
-//$xsl->load('/usr/share/xml/docbook/stylesheet/nwalsh/html/docbook.xsl');
-$xsl->load('http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl');
-//print_r($xsl->saveXML());
+process();
 
-// Configure the transformer
-$proc = new XSLTProcessor;
-$proc->importStyleSheet($xsl); // attach the xsl rules
-
-
-//file_put_contents('test.html', $proc->transformToXml($xml));
-
-
-
-$book = $xml->getElementById('tm');
-
-print_r($book);
-
-print_r(gettype($book));
-
-file_put_contents('test-book.html', $proc->transformToXml($book));
-
-
-//new ReflectionClass($book);
-
+print number_format(memory_get_peak_usage()) . PHP_EOL;
 
 
