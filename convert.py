9d625b24dd4eeb12cf2a87dda633c471c78fcfea
Joe P <stfzebra@gmail.com>
2015-07-21 21:40:00 -0400

    Initial Docbook to Markdown conversion script

---
+++ convert.py
@@ -0,0 +1,13 @@
+''' Conversion script for migrating Library's Docbook XML to Markdown, and
+eventually straight into a fixture. Requires pandoc on the system.'''
+
+import os
+from subprocess import call
+
+for root, dirs, files in os.walk('src'):
+    for file in files:
+        if file.endswith(".xml"):
+           mdfile = file.replace(".xml", ".md")
+           call(["pandoc", "-s", "-f", "docbook", "-t", "markdown", "-o",
+           os.path.join(root, mdfile), os.path.join(root, file)])
+
