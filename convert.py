aebc9c30466d66896818f7ff75139e83a40f82ea
Joe P <stfzebra@gmail.com>
2015-08-05 20:47:30 -0400

    Make the promise of a fixture less absolute, since I'm not sure it's actually worth the effort for a one-time conversion

--- convert.py
+++ convert.py
@@ -1,14 +1,19 @@
 ''' Conversion script for migrating Library's Docbook XML to Markdown, and
-eventually straight into a fixture. Requires pandoc on the system.'''
+maybe straight into a fixture. Requires pandoc on the system.'''
 
-import os
+import os, json
 from subprocess import call
 
 for root, dirs, files in os.walk('src'):
     for file in files:
         if file.endswith(".xml"):
            mdfile = file.replace(".xml", ".md")
-           call(["pandoc", "-s", "-f", "docbook", "-t", "markdown", 
-           "--template=stflibrary", "-o", os.path.join(root, mdfile), 
-           os.path.join(root, file)])
+           call(["pandoc", 
+            "-s", 
+            "-f", "docbook", 
+            "-t", "markdown", 
+            "--template=stflibrary",
+            "-o", os.path.join(root, mdfile), 
+            os.path.join(root, file)])
+
 
